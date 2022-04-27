5923ce4e28d2381f3bf04570390269a27b378cc0
Joshua Oyewole  (Orisfina) <joshpp2013@gmail.com>
2022-04-27 22:16:56 +0100

    initial files commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  darkMode: 'class',
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+    content: [
+    './index.html',
+    'index.html'
+  ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
